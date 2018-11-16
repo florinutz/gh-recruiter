755fea01d3a07b7f2a126c6d74105bdc096c95d2
Florin <florinutz@gmail.com>
2018-11-16 20:06:20 +0100

    fresh

--- cmd/config.go
+++ cmd/config.go
@@ -13,7 +13,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// repoCmd represents the repo command
+// configCmd generates configs
 var configCmd = &cobra.Command{
 	Use:   "gen-config",
 	Short: "generate config",
